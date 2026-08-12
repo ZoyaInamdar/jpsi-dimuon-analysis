24ce04d35feaedc0c26cc35b259c16dc1d606e3d
ZoyaInamdar <inamdarzoya6@gmail.com>
2026-08-12 10:58:10 +0530

    add dimuon event selection cuts

--- src/selection.py
+++ src/selection.py
@@ -15,10 +15,28 @@ def select_opposite_sign_global_muons(df):
         DataFrame containing only events passing the selection.
     """
 
-    mask = (
-        (df["Q1"] * df["Q2"] < 0)
-        & (df["Type1"] == "G")
+    opposite_sign = df["Q1"]*df["Q2"] < 0
+    
+    global_muons = (
+        (df["Type1"] == "G")
         & (df["Type2"] == "G")
+    )
+
+    pt_cut = (
+        (df["Pt1"]>3.0)
+        & (df["Pt2"]>3.0)
+    )
+
+    eta_cut = (
+        (df["Eta1"].abs()<2.4)
+        & (df["Eta2"].abs()<2.4)
+    )
+
+    mask = (
+        opposite_sign
+        & global_muons
+        & pt_cut
+        & eta_cut
     )
 
     return df[mask]
--- tests/test_selection.py
+++ tests/test_selection.py
@@ -9,6 +9,10 @@ def test_selects_opposite_sign_global_muons():
         "Q2": [-1, 1, 1, 1],
         "Type1": ["G", "G", "T", "G"],
         "Type2": ["G", "G", "G", "G"],
+        "Pt1": [5,5,5,5],
+        "Pt2": [5,5,5,5],
+        "Eta1": [1.0,1.0,1.0,1.0],
+        "Eta2": [1.0,1.0,1.0,1.0],
     })
 
     selected = select_opposite_sign_global_muons(df)
@@ -23,6 +27,10 @@ def test_rejects_same_sign_muons():
         "Q2": [1, -1],
         "Type1": ["G", "G"],
         "Type2": ["G", "G"],
+        "Pt1": [5,5],
+        "Pt2": [5,5],
+        "Eta1": [1.0,1.0],
+        "Eta2": [1.0,1.0],
     })
 
     selected = select_opposite_sign_global_muons(df)
@@ -36,6 +44,38 @@ def test_rejects_non_global_muons():
         "Q2": [-1, 1],
         "Type1": ["T", "G"],
         "Type2": ["G", "T"],
+        "Pt1": [5,5],
+        "Pt2": [5,5],
+        "Eta1": [1.0,1.0],
+        "Eta2": [1.0,1.0],
+    })
+
+    selected = select_opposite_sign_global_muons(df)
+
+    assert len(selected) == 0
+
+def test_rejects_low_pt_muons():
+    df = pd.DataFrame({
+        "Q1": [1],
+        "Q2": [-1],
+        "Type1": ["G"],
+        "Type2": ["G"],
+        "Pt1": [2.9],
+        "Pt2": [5.0],
+        "Eta1": [1.0],
+        "Eta2": [1.0],
+    })
+
+def test_rejects_muons_outside_eta_acceptance():
+    df = pd.DataFrame({
+        "Q1": [1],
+        "Q2": [-1],
+        "Type1": ["G"],
+        "Type2": ["G"],
+        "Pt1": [5.0],
+        "Pt2": [5.0],
+        "Eta1": [2.5],
+        "Eta2": [1.0],
     })
 
     selected = select_opposite_sign_global_muons(df)
